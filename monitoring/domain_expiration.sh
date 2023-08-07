d62667c6c7538d65ed708db570173941503ddc80
ILyaAX <ilya.aksenov@ro.ru>
2023-08-07 17:39:03 +0700

    Added date format definition

--- monitoring/domain_expiration.sh
+++ monitoring/domain_expiration.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 DOMAIN="$1"
-exdate=`whois $DOMAIN | grep -E 'paid|Expire|Expiry' | grep -o -E '[0-9]{4}.[0-9]{2}.[0-9]{2}|[0-9]{2}/[0-9]{2}/[0-9]{4}'`
+wdate=`whois $DOMAIN | grep -E 'paid|Expire|Expiry|Expiration'`
+
+exdate=`echo $wdate | grep -m 1 -o -E '[0-9]{4}.[0-9]{2}.[0-9]{2}'`
+if [[ $exdate == "" ]]; then 
+	exdate=`echo $wdate | grep -m 1 -o -E '[0-9]{2}.[0-9]{2}.[0-9]{4}' | sed -E 's/([0-9]{2}).([0-9]{2}).([0-9]{4})\b/\3-\2-\1/g'`
+fi
+
 expire=$((`date -d "$exdate" '+%s'`))
 today=$((`date '+%s'`))
 leftsec=$(($expire - $today))
